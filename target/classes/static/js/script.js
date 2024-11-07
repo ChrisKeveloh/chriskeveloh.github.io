1c08472dff67239144e57a5857a9cdae785450d1
ChrisKeveloh <145189610+ChrisKeveloh@users.noreply.github.com>
2024-11-07 13:42:25 -0600

    Add files via upload

---
+++ target/classes/static/js/script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://lottie.host/4bd4bc9d-7ef7-413f-8c82-970dd561081a/JGzTvYPFwE.json' // lottie file path
+  })
